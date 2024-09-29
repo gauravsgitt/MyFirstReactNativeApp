d434a01f7c0550388c78af1b96ff4c0bc787fb25
Gaurav Bisht <gaurav.bisht@tothenew.com>
2024-09-29 17:54:17 +0530

    intial setup

--- App.tsx
+++ App.tsx
@@ -2,42 +2,19 @@ import React, { useState } from 'react';
 import styles from './styles';
 
 import {
-  Button,
   SafeAreaView,
-  Text,
   View
 } from 'react-native';
 
-import {
-  CustomButton,
-  CustomPressableButton,
-  CustomTouchableHighlightButton,
-  CustomTouchableOpacityButton
-} from './customComponents';
-
 const App = () => {
 
-  let [title, setTitle] = useState("Press here")
+  
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.innerContainer}>
 
-        <Text style={styles.welcomeText}>Hello React Native User!</Text>
-
-        <CustomButton title={title} setTitle={setTitle} onPress={undefined} />
-
-        <CustomPressableButton title={title} setTitle={setTitle} onPress={undefined} />
-
-        <CustomTouchableOpacityButton title={title} setTitle={setTitle} onPress={undefined} />
-
-        <CustomTouchableHighlightButton title={title} setTitle={setTitle} onPress={undefined} />
-
-        <View style={styles.button}>
-          <Button title='Reset' onPress={() => {
-            setTitle('Press here');
-          }} color={"white"} />
-        </View>
+        
 
       </View>
     </SafeAreaView>
--- styles.js
+++ styles.js
@@ -8,28 +8,6 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
-    },
-    welcomeText: {
-        fontSize: 18,
-        marginBottom: 20
-    },
-    button: {
-        backgroundColor: 'blue',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        borderRadius: 10,
-        marginTop: 20
-    },
-    pressableButton: {
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        borderRadius: 10,
-        marginTop: 20
-    },
-    buttonText: {
-        color: 'white',
-        fontSize: 16,
-        textAlign: 'center'
     }
 });
 
